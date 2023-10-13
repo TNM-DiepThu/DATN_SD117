1f205310e6d9bbbf4637d31caa1383df83e7f2fc
Nam Nguyễn Tiến <namntph23972@fpt.edu.vn>
2023-10-14 02:58:18 +0700

    Update view

--- Data/Serviece/ViewModeService/SanPhamChiTietViewModelService.cs
+++ Data/Serviece/ViewModeService/SanPhamChiTietViewModelService.cs
@@ -1,4 +1,5 @@
-﻿using Bill.Serviece.Implements;
+﻿using AppData.ViewModal.SanPhamChiTietVM;
+using Bill.Serviece.Implements;
 using Bill.Serviece.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,28 +11,102 @@ namespace AppData.Serviece.ViewModeService
 {
     public class SanPhamChiTietViewModelService
     {
-        private readonly ISanPhamChiTietServiece _spctservice;
-        private readonly ISanPhamServiece _spservice;
-        private readonly IMauSacServiece _smacserviece;
-        private readonly IThuongHieuServiece _thservice;
-        private readonly ISizeServiece _sservice;
-        private readonly IDanhMucServiece _dmservice;
-        private readonly IAnhServiece _aservice;
+        private readonly ISanPhamChiTietServiece _spct;
+        private readonly ISanPhamServiece _sanpham;
+        private readonly IMauSacServiece _mausac;
+        private readonly IChatLieuServiece _chatlieu;
+        private readonly ISizeServiece _size;
+        private readonly IDanhMucServiece _danhmuc;
+        private readonly IAnhServiece _anh;
         public SanPhamChiTietViewModelService()
         {
-            _aservice = new AnhServiece();
-            _spctservice = new SanPhamChiTietServiece();
-            _spservice = new SanPhamServiece();
-            _smacserviece = new MauSacServiece();
-            _thservice = new ThuongHieuServiece();
-            _dmservice = new DanhMucServiece();
+            _anh = new AnhServiece();
+            _spct = new SanPhamChiTietServiece();
+            _sanpham = new SanPhamServiece();
+            _mausac = new MauSacServiece();
+            _chatlieu = new ChatLieuServiece();
+            _size = new SizeServiece();
+            _danhmuc = new DanhMucServiece();
         }
-        public List<SanPhamChiTietViewModelService> SanPhamChiTiet()
+        public List<SanPhamChiTietViewModel> GetAll()
         {
-            // viet linQ
-            return new List<SanPhamChiTietViewModelService>();
+            var spct = from a in _spct.GetAll()
+                       join b in _sanpham.GetAll() on a.IDSP equals b.Id
+                       join c in _mausac.GetAll() on a.IdMauSac equals c.Id
+                       join d in _danhmuc.GetAll() on a.IdDanhMuc equals d.Id
+                       join e in _size.GetAll() on a.IdSize equals e.Id
+                       join f in _anh.GetAll() on a.IdAnh equals f.Id
+                       join h in _chatlieu.GetAll() on a.IdChatLieu equals h.Id
+                       select new SanPhamChiTietViewModel
+                       {
+                           Id = a.Id,
+                           DanhMuc = d.TenDanhMuc,
+                           TenSP = b.TenSanPham,
+                           ChatLieu = h.TenChatLieu,
+                           MauSac = c.TenMauSac,
+                           Size = e.SizeName,
+                           Anh = f.Connect,
+                           MaSp = a.MaSp,
+                           SoLuong = a.SoLuong,
+                           GiaBan = a.GiaBan,
+                           MoTa = a.MoTa,
+                           status = a.status,
+                       };
+            return spct.ToList();
 
         }
+        public SanPhamChiTietViewModel GetById(Guid ID)
+        {
+            var spct = from a in _spct.GetAll()
+                       join b in _sanpham.GetAll() on a.IDSP equals b.Id
+                       join c in _mausac.GetAll() on a.IdMauSac equals c.Id
+                       join d in _danhmuc.GetAll() on a.IdDanhMuc equals d.Id
+                       join e in _size.GetAll() on a.IdSize equals e.Id
+                       join f in _anh.GetAll() on a.IdAnh equals f.Id
+                       join h in _chatlieu.GetAll() on a.IdChatLieu equals h.Id
+                       select new SanPhamChiTietViewModel
+                       {
+                           Id = a.Id,
+                           DanhMuc = d.TenDanhMuc,
+                           TenSP = b.TenSanPham,
+                           ChatLieu = h.TenChatLieu,
+                           MauSac = c.TenMauSac,
+                           Size = e.SizeName,
+                           Anh = f.Connect,
+                           MaSp = a.MaSp,
+                           SoLuong = a.SoLuong,
+                           GiaBan = a.GiaBan,
+                           MoTa = a.MoTa,
+                           status = a.status,
+                       };
+            return spct.FirstOrDefault(c => c.Id == ID);
+        }
 
+        public SanPhamChiTietViewModel GetByName(string name)
+        {
+            var spct = from a in _spct.GetAll()
+                       join b in _sanpham.GetAll() on a.IDSP equals b.Id
+                       join c in _mausac.GetAll() on a.IdMauSac equals c.Id
+                       join d in _danhmuc.GetAll() on a.IdDanhMuc equals d.Id
+                       join e in _size.GetAll() on a.IdSize equals e.Id
+                       join f in _anh.GetAll() on a.IdAnh equals f.Id
+                       join h in _chatlieu.GetAll() on a.IdChatLieu equals h.Id
+                       select new SanPhamChiTietViewModel
+                       {
+                           Id = a.Id,
+                           DanhMuc = d.TenDanhMuc,
+                           TenSP = b.TenSanPham,
+                           ChatLieu = h.TenChatLieu,
+                           MauSac = c.TenMauSac,
+                           Size = e.SizeName,
+                           Anh = f.Connect,
+                           MaSp = a.MaSp,
+                           SoLuong = a.SoLuong,
+                           GiaBan = a.GiaBan,
+                           MoTa = a.MoTa,
+                           status = a.status,
+                       };
+            return spct.FirstOrDefault(c => c.TenSP.Contains(name));
+        }
     }
 }
