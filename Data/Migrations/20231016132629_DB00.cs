4004368a247f73cb4195b1aaf2fc431dfb6115ef
LAPTOP-04RP8E0L\NDCT <minhtnph26266@fpt.edu.vn>
2023-10-16 20:28:02 +0700

    update-database

---
+++ Data/Migrations/20231016132629_DB00.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppData.Migrations
+{
+    public partial class DB00 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IdSanPhamChiTiet",
+                table: "gioHangChiTiets",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IdComboChiTiet",
+                table: "gioHangChiTiets",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IdSanPhamChiTiet",
+                table: "gioHangChiTiets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IdComboChiTiet",
+                table: "gioHangChiTiets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
--- Data/model/GioHangChiTiet.cs
+++ Data/model/GioHangChiTiet.cs
@@ -11,11 +11,11 @@ namespace AppData.model
         public Guid Id { get; set; }    
         public int SoLuong { get; set; }
         public decimal DonGia { get; set; }
-        public Guid IdSanPhamChiTiet { get; set; }
-        public SanPhamChiTiet SanPhamChiTiet { get; set; }
+        public Guid? IdSanPhamChiTiet { get; set; }
+        public SanPhamChiTiet? SanPhamChiTiet { get; set; }
         public Guid IdGioHang { get; set; }
         public GioHang GioHang { get; set; }
-        public Guid IdComboChiTiet { get; set; }
-        public ComboChiTiet ComboChiTiet { get; set; }
+        public Guid? IdComboChiTiet { get; set; }
+        public ComboChiTiet? ComboChiTiet { get; set; }
     }
 }
