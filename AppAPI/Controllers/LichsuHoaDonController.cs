a3ceca32bfc04ae8a799a5b977a0c2cdf2602e93
LanHuong191 <huongdtlph25484@fpt.edu.vn>
2023-10-17 22:28:56 +0700

    hoadon

--- AppAPI/Controllers/LichsuHoaDonController.cs
+++ AppAPI/Controllers/LichsuHoaDonController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppData.data;
+using AppData.model;
+using AppData.Serviece.Implements;
+using AppData.Serviece.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +13,62 @@ namespace AppAPI.Controllers
     [ApiController]
     public class LichsuHoaDonController : ControllerBase
     {
-        // GET: api/<LichsuHoaDonController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ILichsuHoaDonService<LichSuHoaDon> _lichsuHoaDonService;
+        private MyDbContext _dbContext = new MyDbContext();
+        private DbSet<LichSuHoaDon> lshd;
+
+        public LichsuHoaDonController()
         {
-            return new string[] { "value1", "value2" };
+            lshd = _dbContext.lichSuHoas;
+            LichsuHoaDonService<LichSuHoaDon> all = new LichsuHoaDonService<LichSuHoaDon>(_dbContext, lshd);
+            _lichsuHoaDonService = all;
         }
 
-        // GET api/<LichsuHoaDonController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET: api/<LichsuHoaDonController>
+        [HttpGet("get-lichsuhoadon")]
+        public IEnumerable<LichSuHoaDon> GetLichsuHoaDon()
         {
-            return "value";
+            
+                return _lichsuHoaDonService.GetAll();
+            
         }
+
+       
 
         // POST api/<LichsuHoaDonController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("create-lichsuhoadon")]
+        public bool CreateLichsuHoaDon(DateTime ngaytao, string nguoitao, string ghichu, Guid idhoadon, Guid idnguoidung)
         {
+            LichSuHoaDon lshd = new LichSuHoaDon();
+            lshd.Id = Guid.NewGuid();
+            lshd.NgayTao = ngaytao;
+            lshd.NguoiTao = nguoitao;
+            lshd.GhiChu = ghichu;
+            lshd.IdHoaDon = idhoadon;
+            lshd.IdNguoiDung = idnguoidung;
+            return _lichsuHoaDonService.AddItem(lshd);
         }
 
         // PUT api/<LichsuHoaDonController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("update-lichsuhoadon")]
+        public bool UpdateLichsuHoaDon(Guid id, DateTime ngaytao, string nguoitao, string ghichu, Guid idhoadon, Guid idnguoidung)
         {
+
+            var lshd = _lichsuHoaDonService.GetAll().First(c => c.Id == id);
+            lshd.NgayTao = ngaytao;
+            lshd.NguoiTao = nguoitao;
+            lshd.GhiChu = ghichu;
+            lshd.IdHoaDon = idhoadon;
+            lshd.IdNguoiDung = idnguoidung;
+            return _lichsuHoaDonService.EditItem(lshd);
         }
 
         // DELETE api/<LichsuHoaDonController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("delete-lichsuhoadon")]
+        public bool Delete(Guid id)
         {
+            var lshd = _lichsuHoaDonService.GetAll().First(c => c.Id == id);
+            return _lichsuHoaDonService.RemoveItem(lshd);
         }
     }
 }
