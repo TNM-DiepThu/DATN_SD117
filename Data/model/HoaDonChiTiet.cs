659299cf6ff10b3f6fa6e62a3e9f0e0211365bcc
LAPTOP-04RP8E0L\NDCT <minhtnph26266@fpt.edu.vn>
2023-10-17 13:13:27 +0700

    db01

--- Data/model/HoaDonChiTiet.cs
+++ Data/model/HoaDonChiTiet.cs
@@ -14,9 +14,9 @@ namespace AppData.model
         public decimal Gia { get; set; }
         public Guid IDHD { get; set; }
         public HoaDon HoaDon { get; set; }
-        public Guid IdCombo { get; set; }
+        public Guid? IdCombo { get; set; }
         public ComboChiTiet comboChiTiet { get; set; }
-        public Guid IdSPCT { get; set; }
+        public Guid? IdSPCT { get; set; }
         public SanPhamChiTiet SanPhamChiTiet { get; set; }
 
     }
